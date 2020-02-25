c1189114aa668f4a18ed4c8277571b5af4be3043
sgtkingo <33989823+sgtkingo@users.noreply.github.com>
2020-02-25 10:42:51 +0100

    upd25022020
    buttons fix - no need external resitor (pullup active)

--- leds.c
+++ leds.c
@@ -1,26 +1,47 @@
 /*
- * File:   newmain.c
+ * File:   PWM.c
  * Author: kater
  *
- * Created on 20. listopadu 2019, 15:25
+ * Created on 7. února 2020, 15:41
  */
-
 
 #include "PICF18LF46K22_ConfigSFR.h"
 
-//defined init fce
+#define LED LATDbits.LATD0
+
+//declared init fce
 void InitDevice(){
-    return;
+    OSCCON=0b01111100; //osc setting, 16 MHz, internal by FOSCH
+    OSCTUNE=0x00; //PLLx4 OFF
+    ANSELD=0x00;
+    TRISD=0x00;
 }
-//defined clear fce
+//declared clear fce
 void ClearDevice(){
-    return;
+    PORTD=0;
+    LATD=0;
 }
-//defined test fce
+//declared test fce
 void TestDevice(){
-    return;
+    delay_ms(1000);
+    LATD=0xFF;
+    delay_ms(1000);
+    LATD=0x00;
 }
 
 void main(void) {
+    InitDevice();
+    ClearDevice();
+    TestDevice();
+
+    while(1){
+        LED=!LED;
+        delay_ms(1000);
+        if(LED){
+            LATD<<=1;
+            LATD|=0x01;
+        }
+        if(PORTD == 0xFF)LATD=0;
+    }
     return;
 }
